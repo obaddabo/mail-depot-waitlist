b8b806ea9b4d6b789fb79737b98a456e1ca65dd2
SIta Dabo <euphoria214rue@gmail.com>
2022-11-07 14:07:03 +0100

    new updates

--- app.js
+++ app.js
@@ -3,16 +3,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-
-
 app.use(express.static('public'))
 app.set("view engine", 'ejs' )
 app.use(express.urlencoded({ extended : true}))
+
+
+
+mongoose.connect('mongodb+srv://engnrobad:engnrobad@bloggr.6qgvbcc.mongodb.net/?retryWrites=true&w=majority', (req, res)=>{
+        console.log('Db Conected');
+     
+})
 
 app.get('/', (req, res)=>{
         res.render('index', { title : "Home"});
 }
 );
+
 
 app.post('/', (req, res)=>{
         req.body.email;
@@ -27,6 +33,10 @@ app.get('/sent', (req, res)=>{
 
 app.get('/privacy', (req,res)=>{
         res.render('privacy')
+})
+
+app.use((req, res)=>{
+                res.render('404', {title: 'Forbidden'})
 })
 
 app.listen(3000, ()=>console.log('server started'))
